76294634c09c4af883dbc427f9c4d3c7c7746163
Achim Schneider <achim.schneider@posteo.de>
2022-08-18 13:28:58 +0200

    add comments

--- qdao-node/audit-pallet/src/lib.rs
+++ qdao-node/audit-pallet/src/lib.rs
@@ -65,9 +65,9 @@ pub mod pallet {
     // Dispatchable functions must be annotated with a weight and must return a DispatchResult.
     #[pallet::call]
     impl<T: Config> Pallet<T> {
-        /// An example dispatchable that takes a singles value as a parameter, writes the value to
-        /// storage and emits an event. This function must be dispatched by a signed extrinsic.
         #[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+        // Signs up a new Auditor
+        // ToDo: Auditor needs to stake tokens, needs to provide hash of porfile markdown
         pub fn sign_up(origin: OriginFor<T>) -> DispatchResult {
             // Check that the extrinsic was signed and get the signer.
             // This function will return an error if the extrinsic is not signed.
