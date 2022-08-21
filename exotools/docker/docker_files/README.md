2ee7f7c4d11fb1a622effcfd36bbfa7dc3d51c73
knockoff <niko@skyline.sbs>
2022-08-21 08:45:48 +0000

    Exotools can now execute, but need to talk to people on how to go forward with the auto-audits.

--- exotools/docker/docker_files/README.md
+++ exotools/docker/docker_files/README.md
@@ -2,3 +2,9 @@
 Dockerfile installs cargo audit and syncs repos.
 
 - [ ] TODO - Write some docs on how to properly execute it
+
+
+## Warning
+Inorder to keep the docker container runninf i had to make it listen to /dev/null
+This is not ideal but, as far as i could find the only other way would be to run something else indefinitly. 
+So do we have anything that should run here forever? some simple script to check on things?
--- exotools/exotool.sh
+++ exotools/exotool.sh
@@ -10,8 +10,17 @@
 
 
 # Process Args
-
-# URL or Path to file?
+# just one at the moment, URL
+# Check if URL is valid
+# what is a valid url, is it a specific file type?
+regex='(https?)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]'
+if [[ $1 =~ $regex ]]
+then 
+   URL=$1
+else
+   echo "Invalid URL"
+   exit 1
+fi
 
 
 
@@ -35,26 +44,34 @@ DATE="$(date +%s)"
 DATE_READABLE=$(date +'%d-%m-%Y_%H-%M-%S')
 
 
-# Check if URL is valid
-# what is a valid url, is it a specific file type?
-regex='(https?)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]'
-if [[ $1 =~ $regex ]]
-then 
-   URL=$1
-else
-   echo "Invalid URL"
-   exit 1
-fi
+function prep_folders {
+  TIMESTAMP_DIR="$SCRIPT_PATH"/audits/"$HASH"/"$DATE_READABLE"/
+  PROGRAM_DIR="$SCRIPT_PATH"/audits/"$HASH"/audit_files/program/
+  REPORT_DIR="$SCRIPT_PATH"/audits/"$HASH"/reports/
 
+  mkdir -p "$TIMESTAMP_DIR" "$PROGRAM_DIR" "$REPORT_DIR"
+
+}
 
 # Downloads the files from the URL
 function get_audit_files {
-  DOWNLOAD_PATH="$SCRIPT_PATH"/audits/"$HASH"/audit_files/program.tar
-  curl -s "$URL" --output "$DOWNLOAD_PATH"
+  
+  # What are we downloading? an archive, a single .rs file?
+  TEMP_PATH="/tmp/qrucial"
+  mkdir -p "$TEMP_PATH"/
+  TEMP_PATH="$TEMP_PATH"/"${URL##*/}" # this gets the final parts of url, hopefully the filename
+  curl -s "$URL" --output "$TEMP_PATH"
+  echo "$TEMP_PATH"
+  # Get hash of file.
+  HASH=$(sha512sum "$TEMP_PATH" | cut -d' ' -f1)
+  DOWNLOAD_PATH="$SCRIPT_PATH"/audits/"$HASH"/audit_files/
+  prep_folders
+  mv "$TEMP_PATH" "$DOWNLOAD_PATH"
 
   # Check if it is a tar file
   # Why are we checking if its a tar?
   # what we are downloading will likely just be a rust file .rs
+
   #if ! { tar ztf "$DOWNLOAD_PATH" || tar tf "$DOWNLOAD_PATH"; } >/dev/null 2>&1; then
   #  echo "$DOWNLOAD_PATH is not a tar file"
   #  exit 1
@@ -62,8 +79,6 @@ function get_audit_files {
 
   echo "ok, lets move to execution..."
 
-  # Get hash of file.
-  HASH=$(sha512sum "$DOWNLOAD_PATH" | cut -d' ' -f1)
 
   # https://unix.stackexchange.com/questions/457117/how-to-unshare-network-for-current-process
 }
@@ -74,67 +89,65 @@ function get_audit_files {
 # > Creates a docker container called auditor, mounted at dir, from the image exotools
 # > starts container
 function docker_prep {
-  TIMESTAMP_DIR="$SCRIPT_PATH"/audits/"$HASH"/"$DATE_READABLE"/
-  PROGRAM_DIR="$SCRIPT_PATH"/audits/"$HASH"/audit_files/program/
-  REPORT_DIR="$SCRIPT_PATH"/audits/"$HASH"/reports/
-
-  mkdir -p "$TIMESTAMP_DIR" "$PROGRAM_DIR" "$REPORT_DIR"
   
   docker build -t exotools "$SCRIPT_PATH"/docker/docker_files/
   
   # This Could be useful, but 99% of the time its wasteful
+  #  > better would be to extract and archive only useful data, IE: .history
   #docker save --output "$SCRIPT_PATH"/docker_images/file.tar exotools
 
   docker create --name="$HASH" -v "$PROGRAM_DIR":/auditdir exotools
   
-
-  # Create Timestamp dir, add json, etc.
-  # Report dir should also get a link or the json.
-  # Create system snaphot o fthe container.
-
 }
 
-function call_logger {
-    # Target HTTP service of --> ../logger_and_reporter/python/lar.py
-    curl -X POST "http://127.0.0.1:9999/notify_logger?key=x7roVhBsiZ18Dg3DX3iCm9pXhXdbZWx2"  # TODO Add correct arguments! What do we pass here? succ/fail/info
-}
 
 # Run the proper comands to generate a report
 function exec_audit {
+   # start the docker so we can later execute commands 
+   docker start "$HASH"
    # docker exec... :
       # > Cargo build
       # > cargo audit
       # > place in correct folders
    
+   # docker exec cargo build
+   ## > im gonna need help on this i think.
+   ## > inorder to build it we need to do a few complicated things automatically.
+   ## > maybe its up to the user to compile it then present our program the Cargo.lock?
 
-   #Build
    
-
-
    # Start an automated audit, save output.
-   docker start -a "$HASH" > "$REPORT_DIR"/report.json # Should come up with a better save method. 
+   docker exec cargo audit "$HASH" > "$REPORT_DIR"/report.json # Should come up with a better save method. 
    cp "$REPORT_DIR"/report.json "$TIMESTAMP_DIR"/
-   # [TODO]  > Save Container to image to tar located in timestamp
-   # [TODO]  > Hash based logs, Generate logs in docker container.
+   # cp or symlink, whatever is better
+}
+
+function call_logger {
+   # Target HTTP service of --> ../logger_and_reporter/python/lar.py
+   curl -X POST "http://127.0.0.1:9999/notify_logger?key=x7roVhBsiZ18Dg3DX3iCm9pXhXdbZWx2"  # TODO Add correct arguments! What do we pass here? succ/fail/info
 }
 
 function check_hash {
    # [TODO] Decide if this is necessary in the current state
    # if it is necessary could we implament it together so i get a better idea of whats going on
+   echo "$HASH"
 }
 
 
 
 ## Check hash? - why is it necessary to check the hash?
 # > i think in its current state it doesent make sense, i might be wrong but:
-# > the process of cunducting an audit (with the hash) is:
+# > the process of conducting an audit (with the hash) is:
 #   > Find a file you want to audit
-#   > download the file
-#   > sha256 it to get a hash
+#   > download the file (manually)
+#   > sha256 it (manually) to get a hash
 #   > use the output to execute this script
 #   > this script then does the exact same command and then returns true (or false which woulden happen?)
 #   > is there a better way to implement this?
-
+# > the way we used it before was completely trusting that the hash was correct,
+#   > it used the hash the user gave to generate a directory \
+#     when it should be based on the hash of the downloaded file
+#   > this removes the point of checking a hash. 
 
 ## Sets up folder structure
 #prep_folder
@@ -144,7 +157,7 @@ get_audit_files
 #call_logger
 ## Setup docker, build image, etc
 docker_prep 
-## Run Auditor
+## Run Auditor, cargo build, cargo audit
 exec_audit
 
 
