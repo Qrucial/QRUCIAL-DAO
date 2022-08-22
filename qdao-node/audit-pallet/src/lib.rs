3817c6ca5b74161e2294cf293e828f1b269050eb
Achim Schneider <achim.schneider@posteo.de>
2022-08-22 18:35:28 +0200

    finish sign_up, new unimplemented extrinsics

--- qdao-node/audit-pallet/src/lib.rs
+++ qdao-node/audit-pallet/src/lib.rs
@@ -18,14 +18,15 @@ mod benchmarking;
 #[frame_support::pallet]
 pub mod pallet {
     use super::*;
-    use frame_support::pallet_prelude::*;
+    use frame_support::{pallet_prelude::*, storage::bounded_vec};
     use frame_system::pallet_prelude::*;
+    use sp_core::H256;
 
     #[derive(Encode, Decode, Default, Clone, PartialEq, TypeInfo, MaxEncodedLen)]
     pub struct AuditorData<Hash, AccountId> {
         score: Option<u32>,
-        profile_description: Hash,
-        approvals: [AccountId; 3],
+        profile_hash: Hash,
+        approved_by: BoundedVec<AccountId, ConstU32<3>>,
     }
 
     /// Configure the pallet by specifying the parameters and types on which it depends.
@@ -74,7 +75,7 @@ pub mod pallet {
         #[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
         // Signs up a new Auditor
         // ToDo: Auditor needs to stake tokens, needs to provide hash of porfile markdown
-        pub fn sign_up(origin: OriginFor<T>) -> DispatchResult {
+        pub fn sign_up(origin: OriginFor<T>, profile_hash: H256) -> DispatchResult {
             // Check that the extrinsic was signed and get the signer.
             // This function will return an error if the extrinsic is not signed.
             // https://docs.substrate.io/v3/runtime/origins
@@ -87,12 +88,27 @@ pub mod pallet {
             );
 
             // Register new Auditor
-            // <AuditorScore<T>>::insert(sender.clone(), None::<u32>);
+            let auditor_data = AuditorData::<H256, T::AccountId> {
+                score: None,
+                profile_hash,
+                approved_by: BoundedVec::with_bounded_capacity(3)
+            };
+            <AuditorScore<T>>::insert(sender.clone(), auditor_data);
 
             // Emit an event.
             Self::deposit_event(Event::SignedUp { who: sender });
             // Return a successful DispatchResultWithPostInfo
             Ok(())
+        }
+
+        #[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+        pub fn update_profile(origin: OriginFor<T>, profile_hash: H256) -> DispatchResult {
+           unimplemented!();  
+        }
+
+        #[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+        pub fn cancel_account(origin: OriginFor<T>) -> DispatchResult {
+           unimplemented!();  
         }
         
         #[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
