f564926606d15fa8ba83b05b284d478c3125a918
Achim Schneider <achim.schneider@posteo.de>
2022-08-24 18:13:36 +0200

    add insufficient stake test

--- qdao-node/audit-pallet/src/tests.rs
+++ qdao-node/audit-pallet/src/tests.rs
@@ -13,7 +13,7 @@ fn sign_up_works() {
 
         // When
         // Sign up a new auditor, should work
-        assert_ok!(AuditRepModule::sign_up(Origin::signed(1), hash, 50));
+        assert_ok!(AuditRepModule::sign_up(Origin::signed(1), hash, 10));
 
         // Then
         let auditor_score = AuditorScore::<Test>::try_get(sender);
@@ -24,20 +24,41 @@ fn sign_up_works() {
 }
 
 #[test]
+fn sign_up_fails_when_stake_too_low() {
+    new_test_ext().execute_with(|| {
+
+        // Given
+        let auditor = Origin::signed(1);
+        let hash = H256::repeat_byte(1);
+        let stake = 50;
+
+        // When
+        let result = AuditRepModule::sign_up(auditor, hash, stake);
+
+        // Then
+        assert_noop!(
+            result,
+            Error::<Test>::InsufficientStake
+        );
+    });
+}
+
+#[test]
 fn correct_error_for_double_sign_up() {
     new_test_ext().execute_with(|| {
 
         // Given
         let auditor1 = Origin::signed(1);
         let auditor2 = Origin::signed(2);
+        let stake = 100;
 
         // When
         // Sign up Auditor, should work
-        assert_ok!(AuditRepModule::sign_up(auditor1.clone(), H256::repeat_byte(1), 100));
+        assert_ok!(AuditRepModule::sign_up(auditor1.clone(), H256::repeat_byte(1), stake));
         //Sign up second (different) auditor, should also work
-        assert_ok!(AuditRepModule::sign_up(auditor2, H256::repeat_byte(1), 100));
+        assert_ok!(AuditRepModule::sign_up(auditor2, H256::repeat_byte(1), stake));
         // Sign up an already signed up auditor, should return an error
-        let result = AuditRepModule::sign_up(auditor1, H256::repeat_byte(1), 100);
+        let result = AuditRepModule::sign_up(auditor1, H256::repeat_byte(1), stake);
 
         // Then
         assert_noop!(
