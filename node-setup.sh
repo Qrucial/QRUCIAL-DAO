c413e397bfb1dd0995cd79f8d382b2e61e79541f
six <51x@keemail.me>
2022-08-29 19:11:56 +0200

    Reorg and setup.sh

---
+++ node-setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# ExoTool for QDAO
+#
+# Description: This script is called setting up a QRUCIAL DAO node
+#
+# Author: QDAO Team
+# License: GNU AFFERO GENERAL PUBLIC LICENSE - Version 3, 19 November 2007
+
+# Don't run as root
+if [ "$EUID" -eq 0 ]
+  then echo "You must not run this as root. Please apply basic hardening."
+  exit
+fi
+
+# Dependency checks
+type git >/dev/null || { echo >&2 "git is missing. please install it." ; exit 1;}
+type cargo >/dev/null || { echo >&2 "cargo is missing. please install it." ; exit 1;}
+type docker >/dev/null || { echo >&2 "docker is missing. please install it." ; exit 1;}
+type python3 >/dev/null || { echo >&2 "python3 is missing. please install it." ; exit 1;}
+type curl >/dev/null || { echo >&2 "curl is missing. please install it." ; exit 1;}
+type sha512sum >/dev/null || { echo >&2 "sha512sum is missing. please install it." ; exit 1;}
+type tmux >/dev/null || { echo >&2 "tmux is missing. please install it." ; exit 1;}
+
+# If dep missing ask for install? TBA
+
+# Get from git (later from IPFS?)
+git clone https://github.com/Qrucial/QRUCIAL-DAO
+cd QRUCIAL-DAO/qdao-node
+
+# Build the node
+cargo build
+
+# Build ExoSys Daemon
+cd ../exosysd/
+cargo build
+
+# Start the node in backgroundtmux
+chomod +x target/debug/qdao-node
+tmux new-session -d -s qdao-node './target/debug/qdao-node --dev'
+sleep 7  # Wait for node start, TBA 
+
+# Start ExoSys Daemon in background/tmux
+tmux new-session -d -s qdao-exosysd './target/debug/qdao-exosysd'
