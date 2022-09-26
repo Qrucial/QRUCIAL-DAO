904dc4971ddb1008d3f1b70e73c309e953fde956
six <51x@keemail.me>
2022-09-26 20:46:05 +0200

    Docker improvements

--- docker/files/scripts/qdao_services.sh
+++ docker/files/scripts/qdao_services.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-tmux new-session -d -s qdao-node '/QRUCIAL-DAO/qdao-node/target/debug/qdao-node --dev'
-tmux new-session -d -s qdao-exosysd '/QRUCIAL-DAO/exosysd/target/debug/qdao-exosysd'
-tmux new-session -d -s qdao-api 'python3 /QRUCIAL-DAO/exotools/lar.py'
+tmux new-session -d -s qdao-node '/opt/data/QRUCIAL-DAO/qdao-node/target/release/qdao-node --dev'
+sleep 10 # Wait til node starts, TODO
+tmux new-session -d -s qdao-exosysd '/opt/data/QRUCIAL-DAO/exosysd/target/release/qdao-exosysd'
+tmux new-session -d -s qdao-api 'python3 /opt/data//QRUCIAL-DAO/exotools/lar.py'
 
 # Required for persistant builds. will never recompile unless these files are deleted
 cp -r /opt/data/* /opt/testing
+sleep 999 # Debugging
