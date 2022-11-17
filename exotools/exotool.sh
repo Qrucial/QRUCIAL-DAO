3dd97b54a75aaedfe1f8f8ee65ba4d97226a51e7
six <51x@keemail.me>
2022-11-17 22:20:26 +0100

    Fix for substrate interface breaking change and minor improvements

--- exotools/exotool.sh
+++ exotools/exotool.sh
@@ -6,38 +6,38 @@
 #
 # Author: QDAO Team
 # License: GNU AFFERO GENERAL PUBLIC LICENSE - Version 3, 19 November 2007
-        
+
 # Dependency checks
 type curl >/dev/null || { echo >&2 "curl is missing. please install it." ; exit 1;}
 type docker >/dev/null || { echo >&2 "docker is missing. please install it." ; exit 1;}
 type keccak256 >/dev/null || { echo >&2 "keccak256 is missing. please install it." ; exit 1;}
 
 # Process Args
-if (( $# != 2 )); then  
+if (( $# != 2 )); then
   >&2 echo "incorrect number of args, use: \n\texotool.sh <URL> <HASH>"
 fi
 
 # Check if URL is valid
 regex='(https?)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]'
 if [[ ! $1 =~ $regex ]]
-then 
+then
   echo "Invalid URL"
   exit 1
 fi
 
-# Step back because it is started from Daemon TODO: change this to fix path
+# Step back because it is started from Daemon TODO: change this to fix path - Idea, we should use ~ and qdao user
 cd ../exotools/
 pwd
 
-# > SCRIPT_PATH  = directory of where the script is at starting at root
-#   > this should be used when doing filesysem changes. if not used the wrong directory might be used
+# > SCRIPT_PATH  = Directory of where the script is at starting at root
+#   > This should be used when doing filesysem changes. If not used the wrong directory might be used
 #     > ie: PWD the user executes the tools when not in the same directory as the script.
 #
 # > DATE_READABLE = Time in a human-readable format, IE: 17_08_2022-16_20_43
-#   > the readable date should be used where its more important for it to be recognisable than efficent
+#   > The readable date should be used where its more important for it to be recognisable than efficent
 #
-# > DATE = Unix time, IE: 
-#   > this should be used when it doesent matter if people can read it.
+# > DATE = Unix time, IE:
+#   > This should be used when it doesent matter if people can read it.
 
 SCRIPT_PATH=$(dirname $(realpath "${BASH_SOURCE:-$0}"))
 DATE="$(date +%s)"
@@ -45,12 +45,11 @@ DATE_READABLE=$(date +'%d-%m-%Y_%H-%M-%S')
 URL=$1
 SUPPLIED_HASH=$2
 
-
-# needs to run after get_audit_files, that function sets the hash
+# Prepare the security audit working folders
 function prep_folders {
-  
+
   if [[ ! $HASH ]]; then echo "Hash is not set, Fix code flow"; exit 1; fi
-  
+
   MOUNTPOINT="$SCRIPT_PATH"/static/"$HASH"
   TIMESTAMP_PATH="$MOUNTPOINT"/reports/"$DATE_READABLE"/
   EXTRACT_PATH="$MOUNTPOINT"/audit_files/extract/
@@ -60,47 +59,46 @@ function prep_folders {
 
 }
 
-
-# Downloads the files from the URL
+# Downloads the files from the URL given by ExosysD
 function get_audit_files {
-  
   echo "Retrieve Audit Files"
   echo ""
 
   PROGRAM_NAME="${URL##*/}"
   TEMP_PATH="/tmp/qrucial"
   mkdir -p "$TEMP_PATH"/
-  TEMP_PATH="$TEMP_PATH"/"$PROGRAM_NAME" # this gets the final parts of url, hopefully the filename
+  TEMP_PATH="$TEMP_PATH"/"$PROGRAM_NAME"   # This gets the final parts of url, should be the filename
   curl -s "$URL" --output "$TEMP_PATH"
   echo "$TEMP_PATH"
 
-  # Get hash of file.
+  # Get hash of file
   HASH=$(cat "$TEMP_PATH" | keccak256)
   prep_folders
   mv "$TEMP_PATH" "$DOWNLOAD_PATH"
 
+  # The file needs to be a tar file
   if ! { tar ztf "$DOWNLOAD_PATH"/"$PROGRAM_NAME" || tar tf "$DOWNLOAD_PATH"/"$PROGRAM_NAME"; } >/dev/null 2>&1; then
     echo "$DOWNLOAD_PATH is not a tar file"
     exit 1
   fi
-  
+
   # Extract file. to program dir (to root directory of the docker/)
   tar xf "$DOWNLOAD_PATH"/"$PROGRAM_NAME" --directory=$EXTRACT_PATH
-
-
-  # https://unix.stackexchange.com/questions/457117/how-to-unshare-network-for-current-process
+  # Idea: https://unix.stackexchange.com/questions/457117/how-to-unshare-network-for-current-process
+  # We don't want to provide network connection to the security audit files/processes.
 }
 
-## Docker prep
+## Prepare the docker environment
 # > Creates a docker container called auditor, mounted at dir, from the image exotools
 # > Creates container
 function docker_prep () {
-  echo "Preping Docker"
+  echo "Preparing Docker"
   echo ""
   if [[ $1 == 1 ]]; then
     docker build -t exotools "$SCRIPT_PATH"/docker/docker_files/
   fi
 }
+
 # Safe crash, stop docker and anything that should be cleaned up.
 function safe_exit () {
   echo "Exiting, stopping running processes..."
@@ -109,7 +107,7 @@ function safe_exit () {
     docker container stop "$HASH"
     if [[ ! ($1 == 1 || $1 == 2) ]]; then return; fi
     echo "Removing docker container"
-    docker container rm "$HASH" 
+    docker container rm "$HASH"
   fi
   if [[ ! $1 == 2 ]]; then return; fi
   echo "removing generated files"
@@ -126,14 +124,14 @@ function to_absolute_dir () {
 
 # Run the proper commands to generate a report
 function exec_audit {
-## docker run $HASH parmiters x y z
+## Docker run $HASH parmiters x y z
   docker run --name="$HASH" -v "$MOUNTPOINT":/exotools exotools \
     /usr/exotools/audit_script.sh \
     -h $HASH \
     -d $DATE \
     -D $DATE_READABLE \
     --debug
-  # Hopefully works...
+  # We should have more error handling here.
 }
 
 function call_logger {
@@ -152,7 +150,7 @@ function check_hash {
   echo ""
 }
 
-# Execute functions
+### Execution logic
 
 ## Download, files from url
 get_audit_files
@@ -161,17 +159,14 @@ check_hash
 ## Sets up folder structure > run after check_hash
 prep_folders
 ## Setup docker, build image, etc
-docker_prep 1 
+docker_prep 1
 ## Run Auditor script bundled in the docker image
 exec_audit
 ## Notify the logger about execution
 call_logger
 
-# Would it be smart to offload some of the logic here into the docker file?
-# we could Have a bash script that gets loaded into the docker
+# Ideas: Would it be smart to offload some of the logic here into the docker file?
+# We could Have a bash script that gets loaded into the docker
 
-
-# exit
+# Exit
 safe_exit 1
-
-## ExecutionLogger --> Monitors for new static!
--- exotools/lar.py
+++ exotools/lar.py
@@ -8,14 +8,22 @@ import os
 from subprocess import run
 from substrateinterface import SubstrateInterface, Keypair
 from substrateinterface.exceptions import SubstrateRequestException
-
+import time
 
 ## Interface for QDAO
-substrate = SubstrateInterface(
-    url="ws://127.0.0.1:9944",
-    ss58_format=42,
-    type_registry_preset='kusama'
-)
+substrate=None
+while substrate==None:
+    try:
+        substrate = SubstrateInterface(
+            url="ws://127.0.0.1:9944",
+            ss58_format=42,
+            type_registry_preset='kusama'
+        )
+        print("Successfullly connected to QDAO chain!")
+    except Exception as e:
+        print("Couldn't connect to QDAO's WebSocket. Trying again in 5 seconds.")
+        time.sleep(5)
+    pass
 
 ## Security keys !! TODO make them unique and stored only in mem !!
 keypair = Keypair.create_from_mnemonic('bottom drive obey lake curtain smoke basket hold race lonely fit walk') # Alice
@@ -23,41 +31,45 @@ api_key = 'x7roVhBsiZ18Dg3DX3iCm9pXhXdbZWx2'
 
 ## Flask app and route
 app=Flask(__name__)
- 
+
+# Route used for testing that the service is running
 @app.route("/",methods=['GET'])
 def index():
     if request.method == 'GET':
         arg_t = request.args.get('test')
         if arg_t == "works":
+            print("[DEBUG] HTTP API works.")
             return jsonify("OK, it works.")
         return jsonify("OK, try ?test=works")
     else:
         return jsonify({'Error':"This is a GET API method"}), 400
 
-# ExoTool calls this
-# curl -X POST "http://127.0.0.1:9999/notify_logger?key=x7roVhBsiZ18Dg3DX3iCm9pXhXdbZWx2?hash=0x3ac225168df54212a25c1c01fd35bebfea408fdac2e31ddd6f80a4bbf9a5f1cb"
+# ExoTool calls this, letting lar.py know some execution has finished
+# curl -X POST "http://127.0.0.1:9999/notify_logger?key=x7roVhBsiZ18Dg3DX3iCm9pXhXdbZWx2&hash=0x9b945af23f0701cddcdb7dabcd72c9c7ffd3a155ace237a084b65460a9d36322&result=1"
 @app.route("/notify_logger", methods=['POST'])
 def notif():
     if request.remote_addr == '127.0.0.1':
         pass
     else:
-        return ""
+        return "IP address not allowed."
     if request.method == 'POST':
         api_key_received = request.args.get('key')
-        hash_received = request.args.get('hash')      # TODO check validity
-        result_received = request.args.get('result')  # TODO be more specific + validity + dont accept req without these
+        hash_received = request.args.get('hash')        # TODO check validity
+        result_received = request.args.get('result')    # TODO be more specific + validity + dont accept req without these
         if api_key == api_key_received:
             pass
         else:
             return jsonify("Wrong API key!")
-        run( [ '/usr/bin/touch', '/tmp/OK' ] )
+        run( [ '/usr/bin/touch', './static/report_sample.pdf' ] )         # TODO, Sample and debugging
 
+        # Call QDAO chain and notify it about the execution and the status
+        print("[DEBUG] Sending extrinsic to QDAO node.")
         call = substrate.compose_call(
         call_module='ExoSys',
         call_function='tool_exec_auto_report',
         call_params={
             'hash': str(hash_received),
-            'result': int(result_received)
+            'result': str(result_received)
         })
 
         extrinsic = substrate.create_signed_extrinsic(call=call, keypair=keypair)
@@ -75,16 +87,16 @@ def notif():
 
 
 # Return static files -> reports
-static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static') 
+static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static')
 @app.route('/dir', methods=['GET'])
 def serve_dir_directory_index():
     return send_from_directory(static_file_dir, 'index.html')
- 
+
 @app.route('/dir/<path:path>', methods=['GET'])
 def serve_file_in_dir(path):
     if not os.path.isfile(os.path.join(static_file_dir, path)):
         path = os.path.join(path, 'index.html')
     return send_from_directory(static_file_dir, path) # Secure against dir trav
- 
+
 if __name__ == '__main__':
-    app.run(debug=True,host='127.0.0.1', port=9999)
+    app.run(debug=False,host='127.0.0.1', port=9999)
