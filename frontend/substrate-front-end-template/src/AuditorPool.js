8797d3b1f74f6b582ad4960e410decde36b74cab
kmildi <keleti.ildi@gmail.com>
2023-05-24 18:07:48 +0200

    Fetching data for auditorpool

--- frontend/substrate-front-end-template/src/AuditorPool.js
+++ frontend/substrate-front-end-template/src/AuditorPool.js
@@ -1,24 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Card, Image, Icon } from 'semantic-ui-react'
-
 import { useSubstrateState } from './substrate-lib'
 
-export default function AuditorPool(props) {
+export default function AuditorPool(props) {  
+  const { api } = useSubstrateState()
+  const [auditors, setAuditors] = useState([])
 
-  /* ----- For demo only ----- */
-  const { keyring } = useSubstrateState()
-  const keyringOptions = keyring.getPairs().map(account => ({
-    key: account.address,
-    value: account.address,
-    username: account.meta.name,
-  }))
-  const demoAuditors = keyringOptions.slice(0, 8)
-  /* ------------- */
-  
-/*   const [auditorsData, setAuditorsData] = useState([])
-
-  const getData=()=>{
-    fetch('', {
+  const unsubAll = []
+  const getData= async()=>{
+    const auditorsData = []
+    await fetch('/auditors', {
       headers : { 
         'Content-Type': 'application/json',
         'Accept': 'application/json'
@@ -26,8 +17,26 @@ export default function AuditorPool(props) {
     }).then(response => {
       return response.json()
     }
-    ).then(data =>{
-      setAuditorsData(data)
+    ).then(data => {
+      data.forEach(auditor => {
+        const query = async (address) => {
+          await api.query.auditModule.auditorMap(
+            address,
+            (result) => {                
+              const auditorClone = auditor.slice()
+              if (!result.isNone) {
+                auditorClone.push(JSON.parse(result.toString()).score)
+                auditorClone.push(result.value.approvedBy.length)
+              }
+              const existingIndex = auditorsData.findIndex((elem) => elem.includes(address))
+              if (existingIndex > -1) auditorsData[existingIndex] = auditorClone
+              else auditorsData.push(auditorClone)
+              setAuditors(auditorsData.slice())
+            } 
+          ).then(unsub => unsubAll.push(unsub))
+        }
+        query(auditor[0])
+      })
     }).catch((err) => {
       console.log(err.message)
       //TODO
@@ -36,27 +45,34 @@ export default function AuditorPool(props) {
 
   useEffect(()=>{
     getData()
-  },[]) */
+    return () => { 
+      for (let i = 0; i < unsubAll.length; i++) {
+        unsubAll[i]();
+      }
+    }
+  },[])
 
-  const auditors = demoAuditors
-
-  return (
-    <Card.Group itemsPerRow={4} stackable style={{height: '255px'}} >
-        {auditors.map((auditor) => (
-          <Card style={{boxShadow: 'none', textAlign:'center'}} key={auditor.key}>
-            <div style={{display:'flex', justifyContent: 'center'}}>
-              {auditor.avatar ?
-                <Image circular 
-                  style={{width: '80%'}}
-                  alt='avatar'
-                  src={auditor.avatar}/>
-                :
-                <Icon name='user' size='huge' color='blue'/>
-              }
-            </div>
-            <Card.Description>{auditor.username}</Card.Description>
-          </Card>
-        ))}
-      </Card.Group>
+  // [0] address, 1 profileHash, 2 name, 3 picUrl, 4 webUrl, 5 bio,
+  // 6 auditsDone; 7 score, 8 approvedBy.length
+  
+  return (         
+    <Card.Group itemsPerRow={4} stackable style={{minHeight: '255px'}} >
+      {auditors.map((auditor) => (
+        <Card style={{boxShadow: 'none', textAlign:'center'}} key={auditor[0]}>
+          <div style={{display:'flex', justifyContent: 'center'}}>
+            {auditor[3] ?
+              <Image circular 
+                style={{width: '80%'}}
+                alt='avatar'
+                src={auditor[3]}/>
+              :
+              <Icon name='user' size='huge' color='blue'/>
+            }
+          </div>
+          <Card.Description>{auditor[2] || 'no name'}</Card.Description>
+          <Card.Meta>{auditor[7]}</Card.Meta>
+        </Card>
+      ))}
+    </Card.Group>
   )
 }
