d2680b03096156c17ccdd1640e3e0a726c4d7dc0
six <51x@keemail.me>
2023-06-30 19:20:29 +0200

    Script improvements

--- node-setup.sh
+++ node-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# ExoTool for QDAO
+# Setup script for QDAO
 #
 # Description: This script is called setting up a QRUCIAL DAO node
 #
@@ -50,27 +50,11 @@ cargo build
 chmod +x target/debug/qdao-exosysd
 cd ../
 
-# Start the node in backgroundtmux
-echo "> Starting the full node services in tmux sessions."
-chmod +x ./qdao-node/target/debug/qdao-node
-tmux new-session -d -s qdao-node './qdao-node/target/debug/qdao-node --dev'
-sleep 7  # Wait for node start
-
-# Start ExoSys Daemon in background/tmux
-tmux new-session -d -s qdao-exosysd './exosysd/target/debug/qdao-exosysd'
-
-# Start the QDAO API
-tmux new-session -d -s qdao-api 'python3 exotools/lar.py'
-
-
-# Print results
-if [ -z "$(pgrep exosysd)" ] # We are checking exosysd, as it only runs in the node runs
-  then
-      echo "> There was an error running node setup. Please check the output above and the logs."
-  else
-      echo "> Your local QDAO dev node has been prepared. Your tmux sessions are the following:"
-      tmux ls
-fi
-
-# Run health check
-python3 node-health-check.py
+# Prompt for starting services
+echo "Do you wish to start a full QDAO node?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) chmod +x node-start.sh; ./node-start.sh;;
+        No ) exit;;
+    esac
+done
